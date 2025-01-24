03f89c20c926ba7109034f1ee43f25f01349e6c3
Traube1000101 <girlscoutcookies420@gmx.net>
2025-01-24 21:23:34 +0100

    feat: implement terminal

--- src/components/TerminalContainer.vue
+++ src/components/TerminalContainer.vue
@@ -1,3 +1,37 @@
+<script lang="ts">
+import { Terminal } from '@xterm/xterm';
+import { FitAddon } from '@xterm/addon-fit';
+import '@xterm/xterm/css/xterm.css'
+
+export default {
+  name: 'XtermTerminal',
+  data() {
+    return {
+      terminal: null,
+    };
+  },
+  mounted() {
+    this.initializeTerminal();
+  },
+  methods: {
+    initializeTerminal() {
+      this.terminal = new Terminal({
+        cursorBlink: true,
+      });
+      const fitAddon = new FitAddon();
+      this.terminal.loadAddon(fitAddon);
+      this.terminal.open(this.$refs.terminalContainer);
+      fitAddon.fit();
+
+      this.terminal.writeln('This is a Terminal :3');
+    },
+  },
+  beforeUnmount() {
+      this.terminal?.dispose();
+  },
+};
+</script>
+
 <template>
-  <h1>Terminal</h1>
+  <div ref="terminalContainer"></div>
 </template>

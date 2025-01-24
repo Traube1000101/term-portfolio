e2569d350f607474dc9041df7f224c81d869c27f
Traube <girlscoutcookies420@gmx.net>
2025-01-24 22:52:39 +0100

    chore: format

--- src/components/TerminalContainer.vue
+++ src/components/TerminalContainer.vue
@@ -1,35 +1,35 @@
 <script lang="ts">
-import { Terminal } from '@xterm/xterm';
-import { FitAddon } from '@xterm/addon-fit';
-import '@xterm/xterm/css/xterm.css'
+  import { Terminal } from '@xterm/xterm';
+  import { FitAddon } from '@xterm/addon-fit';
+  import '@xterm/xterm/css/xterm.css';
 
-export default {
-  name: 'XtermTerminal',
-  data() {
-    return {
-      terminal: null,
-    };
-  },
-  mounted() {
-    this.initializeTerminal();
-  },
-  methods: {
-    initializeTerminal() {
-      this.terminal = new Terminal({
-        cursorBlink: true,
-      });
-      const fitAddon = new FitAddon();
-      this.terminal.loadAddon(fitAddon);
-      this.terminal.open(this.$refs.terminalContainer);
-      fitAddon.fit();
-
-      this.terminal.writeln('This is a Terminal :3');
+  export default {
+    name: 'XtermTerminal',
+    data() {
+      return {
+        terminal: null,
+      };
     },
-  },
-  beforeUnmount() {
+    mounted() {
+      this.initializeTerminal();
+    },
+    methods: {
+      initializeTerminal() {
+        this.terminal = new Terminal({
+          cursorBlink: true,
+        });
+        const fitAddon = new FitAddon();
+        this.terminal.loadAddon(fitAddon);
+        this.terminal.open(this.$refs.terminalContainer);
+        fitAddon.fit();
+
+        this.terminal.writeln('This is a Terminal :3');
+      },
+    },
+    beforeUnmount() {
       this.terminal?.dispose();
-  },
-};
+    },
+  };
 </script>
 
 <template>

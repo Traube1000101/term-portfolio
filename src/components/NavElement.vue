b108e21ee0bb384adb36b2ed765c4edbabb7827a
Traube1000101 <girlscoutcookies420@gmx.net>
2025-01-24 21:16:03 +0100

    perf: implement text-stroke

--- src/components/NavElement.vue
+++ src/components/NavElement.vue
@@ -7,27 +7,14 @@
 
 <template>
   <RouterLink class="nav-element" :to>
-    <div>
       <slot></slot>
-    </div>
-    <div class="pseudo-bold">
-      <slot></slot>
-    </div>
   </RouterLink>
 </template>
 
 <style>
-  .nav-element .pseudo-bold {
-    height: 0;
-    visibility: hidden;
-    overflow: hidden;
-    user-select: none;
-    pointer-events: none;
-    font-weight: bolder;
-  }
 
-  .nav-element.router-link-exact-active div {
-    font-weight: bolder;
+  .nav-element.router-link-exact-active {
+    -webkit-text-stroke: 0.05em var(--color-raspberry);
   }
 
   .nav-element.router-link-exact-active:hover {
